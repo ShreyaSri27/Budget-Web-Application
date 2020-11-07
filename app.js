65646943b60ac9aba486c9778a0f35f18195aee8
Shreya Srivastava <shreyasrivastava627@gmail.com>
2020-11-07 19:51:48 +0530

    Updating the Budget Budget Controller-14

--- app.js
+++ app.js
@@ -3,12 +3,20 @@ var budgetController = (function() {
     this.id = id;
     this.description = description;
     this.value = value;
-  }
+  };
   var Income = function(id, description, value) {
     this.id = id;
     this.description = description;
     this.value = value;
-  }
+  };
+
+  var calculateTotal = function(type) {
+    var sum = 0;
+    data.allItems[type].forEach(function(curr) {
+      sum += curr.value;
+    })
+    data.totals[type] = sum;
+  };
 
   var data = {
     allItems: {
@@ -18,7 +26,9 @@ var budgetController = (function() {
     totals: {
       exp: 0,
       inc: 0,
-    }
+    },
+    budget: 0,
+    percentage: -1,
   };
   return {
     addItem: function(type, des, val) {
@@ -36,9 +46,30 @@ var budgetController = (function() {
       data.allItems[type].push(newItem);
       return newItem;
     },
+
+    calculateBudget: function() {
+      // 1. Calculate the total income and expenses__list
+      calculateTotal('inc');
+      calculateTotal('exp');
+
+      // 2. Calculate the total budget__title
+      data.budget = data.totals.inc - data.totals.exp;
+      //3. Calculate the total percentage of expenditure
+      data.percentage = Math.round((data.totals.exp / data.totals.inc) * 100);
+
+
+    },
+    getBudget: function() {
+      return {
+        budget: data.budget,
+        totalInc: data.totals.inc,
+        totalExp: data.totals.exp,
+        percentage: data.percentage,
+      };
+    },
     testing: function() {
       console.log(data);
-    }
+    },
   }
 })();
 
@@ -46,7 +77,7 @@ var UIController = (function() {
 
     var DOMStrings = {
       inputType: '.add__type',
-      intpuDescription: '.add__description',
+      inputDescription: '.add__description',
       inputValue: '.add__value',
       inputBtn: '.add__btn',
       incomeContainer: '.income__list',
@@ -57,7 +88,7 @@ var UIController = (function() {
 
         return {
           type: document.querySelector(DOMStrings.inputType).value,
-          description: document.querySelector(DOMStrings.intpuDescription).value,
+          description: document.querySelector(DOMStrings.inputDescription).value,
           value: parseFloat(document.querySelector(DOMStrings.inputValue).value),
         }
       },
@@ -72,21 +103,23 @@ var UIController = (function() {
           html = '<div class="item clearfix" id="expense-%id%"><div class="item__description">%description%</div><div class="right clearfix"><div class="item__value">%value%</div><div class="item__percentage">21%</div><div class="item__delete"><button class="item__delete--btn"><i class="ion-ios-close-outline"></i></button></div></div> </div>'
         }
 
+
+        // Replace the placeholder text with some actual data
+
         newhtml = html.replace("%id%", obj.id);
         newhtml = newhtml.replace("%description%", obj.description);
         newhtml = newhtml.replace("%value%", obj.value);
 
 
-
-        // Replace the placeholder text with some actual data
+        // Insert the HTML into the DOM
 
         document.querySelector(element).insertAdjacentHTML('beforeend', newhtml);
 
-        // Insert the HTML into the DOM
+
       },
       clearFields: function() {
         var fields, fieldsArr;
-        fields = document.querySelectorAll(DOMStrings.intpuDescription + ", " + DOMStrings.inputValue);
+        fields = document.querySelectorAll(DOMStrings.inputDescription + ", " + DOMStrings.inputValue);
         fieldsArr = Array.prototype.slice.call(fields);
         // console.log(fieldsArr);
         fieldsArr.forEach(function(current, index, array) {
@@ -113,10 +146,13 @@ var controller = (function(budgetCtrl, UICtrl) {
   }
   var updateBudget = function() {
     // 1. Calculate the budget
+    budgetCtrl.calculateBudget();
 
     // 2. Return the budget
+    var budget = budgetCtrl.getBudget();
 
     //3. Display the budget on the UI
+    console.log(budget);
   }
 
 
@@ -132,7 +168,7 @@ var controller = (function(budgetCtrl, UICtrl) {
       // 4. Bring back the focus on the description, and clear the previously entered fields in the placeholder
       UICtrl.clearFields();
       // 5. Calculate the budget
-
+      updateBudget();
       // 6. Display the budget on the UI
     }
   }
